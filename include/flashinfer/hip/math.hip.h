aeae23fa4ef360ecdb4533a8c87053188d928f71
rtmadduri <rishi.madduri@amd.com>
2025-03-05 19:54:12 +0000

    template functions in math, add unit tests

--- include/flashinfer/hip/math.hip.h
+++ include/flashinfer/hip/math.hip.h
@@ -20,24 +20,27 @@ __forceinline__ __device__ half2 uint32_as_half2(uint32_t x) { return *(half2*)&
 
 __forceinline__ __device__ uint32_t half2_as_uint32(half2 x) { return *(uint32_t*)&x; }
 
+template <typename T>
+__forceinline__ __device__ T ptx_exp2(T x);
+
 /// @brief Wrapper for computing 2 ^ x. We currently do not support a direct equivalent of __exp2f()
 /// @param x Input power to exponentiate  
 /// @return Computes 2 ^ x 
-__forceinline__ __device__ float ptx_exp2(float x) {
+__forceinline__ __device__ float ptx_exp2<float>(float x) {
   return __exp10f(x * __log10f(2.0f)); // Writing 2^x = 10 ^ (x * log_10(2))
 }
 
 /// @brief Wrapper for computing 2 ^ x. We currently do not support a direct equivalent of __exp2f()
 /// @param x Input power to exponentiate  
 /// @return Computes 2 ^ x 
-__forceinline__ __device__ half ptx_exp2(half x) {
+__forceinline__ __device__ half ptx_exp2<half>(half x) {
   return hexp2(x);
 }
 
 /// @brief Wrapper for computing 2 ^ x. We currently do not support a direct equivalent of __exp2f()
 /// @param x Vector of two half dtypes to exponentiate  
 /// @return Computes 2 ^ x 
-__forceinline__ __device__ half2 ptx_exp2(half2 x) {
+__forceinline__ __device__ half2 ptx_exp2<half2>(half2 x) {
   return half2(ptx_exp2(x.x), ptx_exp2(x.y));
 }
 
@@ -55,17 +58,20 @@ __forceinline__ __device__ float ptx_rcp(float x) {
   return 	__frcp_rn(x);
 }
 
+template <typename T>
+__forceinline__ __device__ T shfl_xor_sync(T x, int lane_mask);
+
 /// @brief Perform butterfly shuffle between threads in a warp
 /// @param x The value in the source lane
 /// @param lane_mask The mask to perform thread index xor with
-__forceinline__ __device__ float shfl_xor_sync(float x, int lane_mask) {
+__forceinline__ __device__ float shfl_xor_sync<float>(float x, int lane_mask) {
   return __shfl_xor_sync(0xffffffffffffffffULL, x, lane_mask);  // 0xffffffff ensures the full warp is considered
 }
 
 /// @brief Perform butterfly shuffle between threads in a warp
 /// @param x Vector of two values in the source lane
 /// @param lane_mask The mask to perform thread index xor with
-__forceinline__ __device__ half2 shfl_xor_sync(half2 x, int lane_mask) {
+__forceinline__ __device__ half2 shfl_xor_sync<half2>(half2 x, int lane_mask) {
   return __shfl_xor_sync(0xffffffffffffffffULL, x, lane_mask);  // 0xffffffff ensures the full warp is considered
 }
 
@@ -76,18 +82,21 @@ __forceinline__ __device__ float rsqrt(float x) {
   return __frsqrt_rn(x);
 }
 
+template <typename T>
+__forceinline__ __device__ T tanh(T x);
+
 /// @brief Compute tanhf(x)
 /// @param x Input param - float dtype
 /// @return Returns tanhf(x)
 /// @note ROCm6.3 does not have a fast tanh or instrincs to support this
-__forceinline__ __device__ float tanh(float x) {
+__forceinline__ __device__ float tanh<float>(float x) {
   return tanhf(x);
 }
 
 /// @brief A utility function to compute tanh for half dtype
 /// @param x Input param - half 
 /// @return Hyperbolic tangent of x
-__forceinline__ __device__ half tanh(half x) {
+__forceinline__ __device__ half tanh<half>(half x) {
   half a = hexp(x);
   half b = hexp(-x);
 
@@ -97,7 +106,7 @@ __forceinline__ __device__ half tanh(half x) {
 /// @brief Compute hyperbolic tangent for a vector of two half dtype
 /// @param x Vector of two half dtypes
 /// @return Hyperbolic tangent of x
-__forceinline__ __device__ half2 tanh(half2 x) {
+__forceinline__ __device__ half2 tanh<half2>(half2 x) {
   return half2(tanh(x.x), tanh(x.y));
 }
 
